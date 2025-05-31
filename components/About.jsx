43978c5d93a6b1dcb05a1d4d8307657a4688be60
chsiao <chsiao3@my.centennialcollege.ca>
2025-05-31 00:21:13 -0400

    add resume pdf

--- components/About.jsx
+++ components/About.jsx
@@ -18,6 +18,10 @@ export default function About() {
             <p >
                 Hello there! I'm Chung Pao Hsiao, a passionate software developer with a keen interest in creating innovative solutions. I have a strong background in web development and enjoy working on projects that challenge my skills and creativity. In my free time, I love exploring new technologies and contributing to open-source projects. Let's connect and collaborate on exciting ventures!
             </p>
+
+            <a href="../src/assets/dummy.pdf" target="_blank" rel="noopener noreferrer">
+                Download My Resume (PDF)
+            </a>
         </div>
     );
 }
